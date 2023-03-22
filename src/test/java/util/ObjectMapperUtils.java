83d5b49028d846ca1ba6f96c8ed11c2d897409de
Yahya Yıldırım <eminearigulio@gmail.com>
2023-03-22 23:22:52 +0300

    22/03/20231

--- src/test/java/util/ObjectMapperUtils.java
+++ src/test/java/util/ObjectMapperUtils.java
@@ -5,16 +5,16 @@ import org.codehaus.jackson.map.ObjectMapper;
 import java.io.IOException;
 
 public class ObjectMapperUtils {
-
-    //<T> T ==> Herhangi bir data tipi
-    //ObjectMapper().readValue(json, cls) methodu birinci parametrede aldığı String formatındaki Json datyı ijkinci parametrede belitilen Java objesine çevirir.
-    public static <T> T convertJsonToJava(String json, Class<T> cls) {//Generic Method
+    //        <T> T -> Herhangi bir data tipi demek
+//        ObjectMapper().readValue(json, cls) methodu birinci parametrede aldığı String formatındaki
+//                                   Json datayı ijkinci parametrede belitilen Java objesine çevirir.
+    public static <T> T convertJsonToJava(String json, Class<T> cls){ // Generic Method
 
         try {
-            return new ObjectMapper().readValue(json, cls);
+            return new ObjectMapper().readValue(json,cls);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
 
+    }
 }
